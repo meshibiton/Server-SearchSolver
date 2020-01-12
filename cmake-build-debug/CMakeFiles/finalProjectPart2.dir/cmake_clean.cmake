97dce00ac91f4ced221d4adced435c9d431e90cc
meshibiton <meshi19@gmail.com>
2020-01-13 00:57:11 +0200

    first time up

---
+++ cmake-build-debug/CMakeFiles/finalProjectPart2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/finalProjectPart2.dir/main.cpp.o"
+  "finalProjectPart2"
+  "finalProjectPart2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/finalProjectPart2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
