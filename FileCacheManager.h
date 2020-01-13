b179de15190363604f815402947da27014f8bec2
raph <raphaelhouri789@gmail.com>
2020-01-13 02:27:47 +0200

    update

---
+++ FileCacheManager.h
@@ -0,0 +1,16 @@
+//
+// Created by raph on 1/13/20.
+//
+
+#ifndef FINALPROJECTPART2_FILECACHEMANAGER_H
+#define FINALPROJECTPART2_FILECACHEMANAGER_H
+
+#include "CacheManager.h"
+
+
+class FileCacheManager:public CacheManager{
+
+};
+
+
+#endif //FINALPROJECTPART2_FILECACHEMANAGER_H
